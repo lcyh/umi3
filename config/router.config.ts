fcc6b3327c122a3005d093ea9706f7a6a4df61f3
luochang <luochang@bilibili.com>
2021-02-23 14:42:51 +0800

    feat: 🎸 添加 登录页路由

--- config/router.config.ts
+++ config/router.config.ts
@@ -2,6 +2,14 @@ import routes from './routes';
 
 export default [
   {
+    name: '登录',
+    icon: 'login',
+    iconfont: 'login',
+    path: '/login',
+    component: './Login',
+    exact: true,
+  },
+  {
     path: '/',
     component: '../layouts/BasicLayout',
     routes: routes
